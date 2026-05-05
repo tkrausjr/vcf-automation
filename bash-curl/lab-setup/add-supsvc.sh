d20cf8d080495f31de50808b6378ce905eb5f234
tkrausjr <tkrausjr@gmail.com>
2026-05-05 13:29:59 -0700

    Added kubectl add and commands and working

--- bash-curl/lab-setup/add-supsvc.sh
+++ bash-curl/lab-setup/add-supsvc.sh
@@ -5,24 +5,22 @@
 ###################################################
 
 VCENTER_VERSION=9
+HEADER_CONTENTTYPE="Content-Type: application/json"
+SUPERVISOR_VIP=10.1.0.6
 
 ###################################################
 # Import .env Shell Variables
-###################################################
 # Obtain sensitive information from .env file in the folder where script will run
+###################################################
 if [ -f .env ]; then  # Automatically export all variables defined in .env
   set -a
     source .env
 	  set +a
 	  fi
-
 	  echo "Your Target vCenter is: $VCENTER_HOSTNAME"
 
 ###################################################
 
-###################################################
-
-HEADER_CONTENTTYPE="Content-Type: application/json"
 ################################################
 # 0 - Login to VCenter and get Session ID
 ###############################################
@@ -36,13 +34,13 @@ echo Authenticated successfully to VC with Session ID - ${SESSION_ID} ...
 HEADER_SESSIONID="vmware-api-session-id: ${SESSION_ID}"
 
 echo "Getting Supervisor ID"
-SUPERVISOR_ID=$(curl -sk -X GET 'https://vc-wld01-a.site-a.vcf.lab/api/vcenter/namespace-management/supervisors/summaries' -H "${HEADER_SESSIONID}" -H 'Content-Type: application/json' | jq -r '.items[0].supervisor')
+SUPERVISOR_ID=$(curl -sk -X GET https://${VCENTER_HOSTNAME}/api/vcenter/namespace-management/supervisors/summaries -H "${HEADER_SESSIONID}" -H 'Content-Type: application/json' | jq -r '.items[0].supervisor')
 echo $SUPERVISOR_ID
-
-# read -p "Press [Enter] to continue . . . "
+# TODO - Find way to return Supervisor Control Plane (LB VIP) to use wqith VCF CLI.
+#curl -k -X GET https://${VCENTER_HOSTNAME}/api/vcenter/namespace-management/supervisors -H "${HEADER_SESSIONID}" -H 'Content-Type: application/json' | jq -r '.'
 
 # Below Temporary Exclusion of Supervisor Services code to test vSphere Namespace addition
-: <<'COMMENT'
+# : <<'COMMENT'
 ################################################
 # 1 - Adding Supervisor Services to vCenter
 
@@ -61,8 +59,6 @@ for filename in supsvc-*.*ml; do
 	echo "Adding Supervisor Service to ${VCENTER_HOSTNAME}  ..."
 	curl -ks -X POST -H "${HEADER_SESSIONID}" -H "${HEADER_CONTENTTYPE}" -d "@temp_final.json" https://${VCENTER_HOSTNAME}/api/vcenter/namespace-management/supervisor-services
 done
-echo "Sleeping 10 seconds..."
-sleep 10
 
 ################################################
 # 2 - Adding new Supervisor Services to the Supervisor
@@ -78,7 +74,10 @@ ARGO_SERVICE_SPEC='{"supervisor_service": "argocd-service.vsphere.vmware.com","v
 echo $ARGO_SERVICE_SPEC
 curl -k -X POST -H "${HEADER_SESSIONID}" -H "${HEADER_CONTENTTYPE}" -d "${ARGO_SERVICE_SPEC}" https://${VCENTER_HOSTNAME}/api/vcenter/namespace-management/supervisors/${SUPERVISOR_ID}/supervisor-services
 
-COMMENT
+echo "Sleeping 60 seconds for Supervisor Services Operators and webhooks to come online ..."
+sleep 60
+
+#COMMENT
 # Above TEMPORARY for debugging to comment out all Supervisor Services 
 ###################################################
 echo -e "\n Listing Supervisor Services"
@@ -106,12 +105,35 @@ echo -e "\nNew vSphere Namespace Creation status: $NAMESPACES_STATUS"
 echo -e "New vSphere Namespace Creation Message: $NAMESPACES_MESSAGE" 
 
 ###################################################
-## 5 - List Namespaces
+## 5 - List Namespaces again to check for newly create namespace
 VSPHERE_NAMESPACES=$(curl -sk -X GET -H "${HEADER_SESSIONID}" -H "${HEADER_CONTENTTYPE}" https://${VCENTER_HOSTNAME}/api/vcenter/namespaces/instances/v2)
 echo -e "\n Listing current vSphere Namespaces by Name. \n"
 echo "$VSPHERE_NAMESPACES" | jq -r '.[].namespace'
 
 ###################################################
-## 6 - Adding ArgoCD Instance to the newly created vSphere Namespace
-## Will need to do things with vcf CLI and then apply the manifest for ArgoCD Instance - from my VKS repo in supervisorservices directory.
+## 6. VCF CLI Setup ---
+echo "\nPre-configuring VCF CLI (EULA, CEIP, and plugins)..."
+export VCF_CLI_VSPHERE_PASSWORD=$VCENTER_PASSWORD
+vcf plugin sync 2>/dev/null || true
+vcf telemetry update --opted-out 2>/dev/null || true
 
+echo "Creating VCF Supervisor Context..."
+vcf context create supervisor-ctx \
+  --endpoint "$SUPERVISOR_VIP" \
+  --username "$VCENTER_USERNAME" \
+  --insecure-skip-tls-verify \
+  -t kubernetes \
+  --auth-type basic 2>/dev/null || echo "Context may already exist. Continuing..."
+
+echo "Setting supervisor-ctx as current context..."
+vcf context use supervisor-ctx 2>/dev/null || true
+
+###################################################
+## Testing kubectl
+echo "\nTesting kubectl Authentication"
+kubectl get ns
+
+###################################################
+## 7 - Adding ArgoCD Instance to the newly created vSphere Namespace
+## Will need to do things with vcf CLI and then apply the manifest for ArgoCD Instance - from my VKS repo in supervisorservices directory.
+kubectl apply -f argocd-instance.yaml -n shared-svcs
